d4ef9165b56a32713c96d088d5aab50fe3719145
juana <juana@DESKTOP-EAGMBMR>
2022-05-22 19:39:30 -0500

    Agregar archivos de proyecto.

---
+++ Entidad/Cuenta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Cuenta
+    {
+        public Cuenta(double numeroCuenta, Cliente cliente, double saldo)
+        {
+            NumeroCuenta = numeroCuenta;
+            Cliente = cliente;
+            Saldo = saldo;
+        }
+
+        public double NumeroCuenta { get; set; }
+        public Cliente Cliente { get; set; }
+        private double Saldo;
+        public double getSaldo()
+        {
+            return Saldo;
+        }
+        public string Consignar(double valor)
+        {
+            Saldo += valor;
+            return "Consignacion exitosa";
+        }
+        public string Retirar(double valor)
+        {
+            if (Saldo < valor)
+            {
+                return "Fondos insuficiente";
+            }
+            Saldo -= valor;
+            return "Retiro exitoso";
+        }
+        public override string ToString()
+        {
+            return NumeroCuenta.ToString() + ";" + Cliente.IdCliente + ";" + Saldo.ToString();
+        }
+    }
+}
